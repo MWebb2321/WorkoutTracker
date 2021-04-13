cbc04bf354be9fa7e4914b8fd1181e622f8c8bf1
Matthew Webb <mwebb2321@gmail.com>
2021-04-13 16:47:56 -0700

    added two get routes for stats and exercises

---
+++ controllers/html-routes.js
@@ -0,0 +1,12 @@
+path = require("path");
+
+//Generate response to Path request
+module.exports = function (app) {
+  app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+  });
+
+  app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercises.html"));
+  });
+};
