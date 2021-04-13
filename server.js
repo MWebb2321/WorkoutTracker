ad15b72fdc5a89545e369b6a664290ab5fc7a217
Matthew Webb <mwebb2321@gmail.com>
2021-04-13 16:07:13 -0700

    began work on server

---
+++ server.js
@@ -0,0 +1,30 @@
+// Boilerplate express app with Mongo
+// Required modules
+const express = require("express");
+const mongoose = require("mongoose");
+
+// Establish port
+const PORT = process.env.PORT || 3010;
+
+// Set up Express app
+const app = express();
+
+// Allow URL processing and JSON
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
+// Set up Static folder
+app.use(express.static("public"));
+
+// Connect to Mongoose database
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+});
+
+// Set up Routes
+require("./controllers/html-routes.js")(app);
+require();
